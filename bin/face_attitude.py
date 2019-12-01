9a116dae683b861c5cce073c724a417ac46d4f02
LinJacck <57176639+linjacck@users.noreply.github.com>
2019-12-01 21:29:44 +0800

    增添了姿态识别

--- bin/face_attitude.py
+++ bin/face_attitude.py
@@ -1,15 +1,20 @@
 import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-def face_attitude(key,secret,face_tokens):
-    url = 'https://api-cn.faceplusplus.com/facepp/v3/face/analyze'
+
+def face_attitude(key,secret,face):
+    url = 'https://api-cn.faceplusplus.com/facepp/v3/detect'
     params = {
         'api_key': key,
         'api_secret': secret,
-        'face_tokens': face_tokens,
         'return_landmark':0,
         'return_attributes':'gender,age,smiling,headpose,emotion,beauty,skinstatus'
     }
-    r = requests.post(url, data=params)
+
+    r = requests.post(url, data=params, files=face)
+    print(r)
     req_dict = r.json()
     print(req_dict)
+    return req_dict
+
 
--- start/AttitudeDistinguish.py
+++ start/AttitudeDistinguish.py
@@ -1,8 +1,28 @@
+import os
+
+import cv2
+
 from bin.face_attitude import face_attitude
+from common.untrusted_draw import UntrustedDraw
 
 key = "OtLBlW7lShY1Wsgra5g6xNEHA400Fumv"
 secret = "L12X1KWMbF1F3t5XPO1WChhgSzQSiA8p"
 
-face_tokens = '1c8ca96278ac8a10eb97773d8145d6d3'
+# face_tokens = '1c8ca96278ac8a10eb97773d8145d6d3'
+data_file = os.path.abspath('..')#项目路径
+faces = os.path.join(data_file,'..','faces')#faces路径，face要放在项目的同一级
 
-face_attitude(key,secret,face_tokens)
+# for i in range(32,37):
+# #     face_name = str(i) + '.jpg'
+# #     face_file = os.path.join(faces,face_name)
+# #     face = {"image_file": open(face_file, "rb")}
+# #     print(face)
+# #     face_information = face_attitude(key,secret,face)#包括姿态、位置在内的各类信息
+# #     UntrustedDraw(face_information,face_file)
+
+face_name = '03' + '.jpeg'
+face_file = os.path.join(faces,face_name)
+face = {"image_file": open(face_file, "rb")}
+print(face)
+face_information = face_attitude(key,secret,face)#包括姿态、位置在内的各类信息
+UntrustedDraw(face_information,face_file)
